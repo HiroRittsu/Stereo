f763745dab92fdfcdc3e3177af830b8d944ae221
MiglyA <zq6295migly@gmail.com>
2018-09-20 20:31:37 +0900

    stereo.hppにコメントアウト

--- src/Stereo.hpp
+++ src/Stereo.hpp
@@ -28,8 +28,6 @@ private:
 	int TEMP_W;
 	double T;
 	double f;
-
-
 };
 
 Stereo::Stereo() {
@@ -38,24 +36,23 @@ Stereo::Stereo() {
 	this->TEMP_W = 3;
 	this->T = 10;
 	this->f = 7.5;
-
 }
 
 Stereo::~Stereo() {
 
 }
 
+//SAD類似度計算
 double Stereo::getSimilarity(cv::Mat image1, cv::Point image1_point, cv::Mat image2, cv::Point image2_point, int size[]) {
 
 	double sum = 0;
 
 	for (int y = 0; y < size[0]; ++y) {
+		
 		for (int x = 0; x < size[1]; ++x) {
 
 			sum += abs(image1.at<unsigned char>(image1_point.y + y, image1_point.x + x) - image2.at<unsigned char>(image2_point.y + y, image2_point.x + x));
-
 		}
-
 	}
 
 	return sum;
@@ -73,6 +70,7 @@ cv::Point Stereo::matchPoint(cv::Mat left, cv::Mat right, cv::Point target) {
 	//将来的にはエピポーラ線も考慮する
 	for (int x = 0; x <= target.x - ((TEMP_W - 1) / 2); x += 3) { //rightimage
 
+		//類似度計算
 		sim = this->getSimilarity(left, cv::Point(target.x - ((TEMP_W - 1) / 2), target.y - ((TEMP_H - 1) / 2)), right, cv::Point(x, target.y - ((TEMP_H - 1) / 2)), size);
 
 		if (sim_min > sim) {
@@ -103,12 +101,14 @@ cv::Mat Stereo::getDistanceMap(cv::Mat left, cv::Mat right, double *max, double 
 	*max = 0;
 	*min = std::numeric_limits<double>::max();
 
+	//オリジナル画像すべての画素を参照
 	for (int y = (TEMP_H - 1) / 2; y < left.size[0] - ((TEMP_H - 1) / 2); ++y) {
 
 		//timer_hpp.startTime();
 
 		for (int x = (TEMP_W - 1) / 2; x < left.size[1] - ((TEMP_W - 1) / 2); ++x) {
 
+			//オリジナル画像のある画素と一致する場所を計算
 			result = this->matchPoint(left, right, cv::Point(x, y));
 
 			if (result.x != -1) {
@@ -120,16 +120,9 @@ cv::Mat Stereo::getDistanceMap(cv::Mat left, cv::Mat right, double *max, double 
 					if (*max < result_distance) *max = result_distance;
 					if (*min > result_distance) *min = result_distance;
 				}
-
-
 			}
-
 		}
-
-		//printf("%f\n",timer_hpp.getTime() );
-
 	}
-
 	return map;
 }
 
